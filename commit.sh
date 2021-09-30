5b9f723847fa984b0b7f0cecbdd56dec758c0783
zx4321 <543977392@qq.com>
2021-09-30 20:30:52 +0800

    test2

--- commit.sh
+++ commit.sh
@@ -1,9 +1,12 @@
+book sm
+gitbook build
 git add .
 git commit -m $1
+git push origin
+git branch -D gh-pages
 git checkout -b gh-pages
-gitbook build
 git add .
 git commit -m $1
 git subtree push --prefix=_book -f origin gh-pages
 git checkout master
-git branch  -D gh-pages
+git branch -D gh-pages
