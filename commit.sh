0f93f3dcca33ea613a77e4cccd773125c3ada369
zx4321 <543977392@qq.com>
2021-09-30 20:46:31 +0800

    test4

--- commit.sh
+++ commit.sh
@@ -8,6 +8,7 @@ git checkout -b gh-pages
 echo ".idea/" > .gitignore
 git add .
 git commit -m $1
+git push origin --delete gh-pages
 git subtree push --prefix=_book origin gh-pages
 git checkout master
 git branch -D gh-pages
