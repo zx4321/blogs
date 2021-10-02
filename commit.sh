c346c01432ec3a25be156f3bf05b2cc32295f09f
zx4321 <543977392@qq.com>
2021-10-02 18:56:45 +0800

    start

---
+++ commit.sh
@@ -0,0 +1,16 @@
+# 自动生成目录
+book sm
+# 打包
+gitbook build
+git add .
+git commit -m $1
+git push origin
+git branch -D gh-pages
+git checkout -b gh-pages
+echo ".idea/" > .gitignore
+git add .
+git commit -m $1
+git push origin --delete gh-pages
+git subtree push --prefix=_book origin gh-pages
+git checkout master
+git branch -D gh-pages
